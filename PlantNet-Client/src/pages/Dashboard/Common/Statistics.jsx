a6ab7c154f2f155c209cd4d54ea808cb60053a55
MukitHossen7 <hossenmukit7@gmail.com>
2025-01-08 11:33:16 +0600

    create admin and seller private route

--- PlantNet-Client/src/pages/Dashboard/Common/Statistics.jsx
+++ PlantNet-Client/src/pages/Dashboard/Common/Statistics.jsx
@@ -1,14 +1,21 @@
-import { Helmet } from 'react-helmet-async'
-import AdminStatistics from '../../../components/Dashboard/Statistics/AdminStatistics'
+import { Helmet } from "react-helmet-async";
+import AdminStatistics from "../../../components/Dashboard/Statistics/AdminStatistics";
+import useRole from "../../../hooks/useRole";
+import { Navigate } from "react-router-dom";
 const Statistics = () => {
+  const [role] = useRole();
+  if (role === "customer")
+    return <Navigate to="/dashboard/my-orders"></Navigate>;
+  if (role === "seller")
+    return <Navigate to="/dashboard/my-inventory"></Navigate>;
   return (
     <div>
       <Helmet>
         <title>Dashboard</title>
       </Helmet>
-      <AdminStatistics />
+      {role === "admin" && <AdminStatistics />}
     </div>
-  )
-}
+  );
+};
 
-export default Statistics
+export default Statistics;
